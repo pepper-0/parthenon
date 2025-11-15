480ebc459039475d8609236f006a8ff8d34b418c
Anahita Mayekar <anahitamayekar03@gmail.com>
2025-11-15 10:23:18 -0500

    Connected buttons to scenes

---
+++ home/home.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _on_brooklyn_pressed() -> void:
+	get_tree().change_scene_to_file("res://home/home.tscn")
+
+
+func _on_liberty_pressed() -> void:
+	get_tree().change_scene_to_file("res://home/home.tscn")
+
+
+func _on_park_pressed() -> void:
+	get_tree().change_scene_to_file("res://home/home.tscn")
+
+
+func _on_ts_pressed() -> void:
+	get_tree().change_scene_to_file("res://home/home.tscn")
