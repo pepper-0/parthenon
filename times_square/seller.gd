2279cf52ad66fe04a002b54895cf420d5bc77fe0
mayalh22 <maya.l.hazarika@gmail.com>
2025-11-16 03:40:29 -0500

    added ending game screen

--- times_square/seller.gd
+++ times_square/seller.gd
@@ -2,6 +2,6 @@ extends Node2D
 
 func _on_seller_body_entered(body: Node2D) -> void:
 	if body is Player and body.coin_counter >= 10:
-		get_tree().change_scene_to_file("res://end/end.tscn")
+		get_tree().change_scene_to_file("res://win.tscn")
 		var has_helmet = true
 		self.queue_free()
