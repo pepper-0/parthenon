fbd712b0a8130c76357bda32c5365302f3653293
mayalh22 <maya.l.hazarika@gmail.com>
2025-11-16 06:10:08 -0500

    fixed error abt liberty - redid file

--- home/home.gd
+++ home/home.gd
@@ -3,14 +3,11 @@ extends Control
 func _on_brooklyn_pressed() -> void:
 	get_tree().change_scene_to_file("res://brooklyn-bridge.tscn")
 
-
 func _on_liberty_pressed() -> void:
 	get_tree().change_scene_to_file("res://statue-of-liberty.tscn")
 
-
 func _on_park_pressed() -> void:
 	get_tree().change_scene_to_file("res://park/park.tscn")
-
 
 func _on_ts_pressed() -> void:
 	get_tree().change_scene_to_file("res://times_square/times_square.tscn")
