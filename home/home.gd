7a440b7cc90085d61a595a5a342aff6f9b83c127
Anahita Mayekar <anahitamayekar03@gmail.com>
2025-11-15 19:24:24 -0500

    commit

--- home/home.gd
+++ home/home.gd
@@ -13,4 +13,4 @@ func _on_park_pressed() -> void:
 
 
 func _on_ts_pressed() -> void:
-	get_tree().change_scene_to_file("res://home/home.tscn")
+	get_tree().change_scene_to_file("res://times_square.tscn")
