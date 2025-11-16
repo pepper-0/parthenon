854f45b785e1099a7d3e6dd3b289e779ac58b827
mayalh22 <maya.l.hazarika@gmail.com>
2025-11-16 03:34:22 -0500

    yay

--- times_square/seller.gd
+++ times_square/seller.gd
@@ -3,4 +3,5 @@ extends Node2D
 func _on_seller_body_entered(body: Node2D) -> void:
 	if body is Player and body.coin_counter >= 10:
 		get_tree().change_scene_to_file("res://end/end.tscn")
+		var has_helmet = true
 		self.queue_free()
